1c87460ee3a7e5dd9e8f7b2db005469bbffa4978
Vano <glazglojon@gmail.com>
2024-12-18 13:26:16 +0200

    Python SQLite #2: підключення до БД, створення та видалення таблиці

---
+++ ex1.py
@@ -0,0 +1,15 @@
+import sqlite3 as sq
+
+with sq.connect('saper.db') as con: #контекст менеджер відкриває БД, функція connect встановлює звязок з БД
+    cur = con.cursor() #виконується робота з БД
+
+    cur.execute("DROP TABLE IF EXISTS users") #видаленння таблоиці
+    cur.execute("""CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL DEFAULT 1,
+    sex INTEGER NOT NULL DEFAULT 1,
+    old INTEGER,
+    score INTEGER
+    )""")# передається SQL запит до БД
+
+
